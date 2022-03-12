9faedf159cb9fd04843e73c0fd28c74fb7d0e139
Kanha Tomar <kanharajput91@gmail.com>
2022-03-12 17:23:53 +0530

    Hide one View and show Another with ProgressBar

--- app/src/main/java/com/example/revealhideview/MainActivity.java
+++ app/src/main/java/com/example/revealhideview/MainActivity.java
@@ -9,10 +9,55 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View contentView;                     // to reference the scrollview
+    private View loadingView;                    // to reference the progressview
+    private int shortAnimationDuration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        contentView  = findViewById(R.id.content);
+        loadingView = findViewById(R.id.loading_spinner);
+
+        // Initially hide the content view.
+        contentView.setVisibility(View.GONE);
+
+        // Retrieve and cache the system's default "short" animation time.
+        shortAnimationDuration = getResources().getInteger(
+                android.R.integer.config_longAnimTime);
+
+        // call the crossFade method
+        crossfade();
+
+    }
+
+    private void crossfade() {
+
+        // Set the content view to 0% opacity but visible, so that it is visible
+        // (but fully transparent) during the animation.
+        contentView.setAlpha(0f);
+        contentView.setVisibility(View.VISIBLE);            // I think alpha value is 0f so we are directly make it visible so it further step we only need to change the alpha
+
+        // Animate the content view to 100% opacity, and clear any animation
+        // listener set on the view.
+        contentView.animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration)
+                .setListener(null);
+
+        // Animate the loading view to 0% opacity. After the animation ends,
+        // set its visibility to GONE as an optimization step (it won't
+        // participate in layout passes, etc.)
+        loadingView.animate()
+                .alpha(0f)
+                .setDuration(shortAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        loadingView.setVisibility(View.GONE);
+                    }
+                });
     }
 }
