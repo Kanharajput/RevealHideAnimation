93ff4e0fafcec23deda723dfea92a4b6ee821d52
Kanha Tomar <kanharajput91@gmail.com>
2022-03-12 17:03:38 +0530

    Started with Empty Activity

---
+++ app/src/main/java/com/example/revealhideview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.revealhideview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
