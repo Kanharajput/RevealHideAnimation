bc23bf4958f6920e473ca0ccd963b92bf77957f7
Kanha Tomar <kanharajput91@gmail.com>
2022-03-13 17:43:35 +0530

    Reveal or Hide a view

--- app/src/main/java/com/example/revealhideview/MainActivity.java
+++ app/src/main/java/com/example/revealhideview/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private View contentView;                     // to reference the scrollview
     private View loadingView;                    // to reference the progressview
     private int shortAnimationDuration;
+    private View floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         contentView  = findViewById(R.id.content);
         loadingView = findViewById(R.id.loading_spinner);
+        floatingActionButton = findViewById(R.id.fab);
 
         // Initially hide the content view.
         contentView.setVisibility(View.GONE);
@@ -66,4 +72,62 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,CardFlipAnimationActivity.class);
         startActivity(intent);
     }
+
+    // hiding the fab button
+    public void hideFab(View view) {
+
+        // Check if the runtime version is at least Lollipop
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // get the center for the clipping circle
+            int cx = floatingActionButton.getWidth() / 2;
+            int cy = floatingActionButton.getHeight() / 2;
+
+            // get the final radius for the clipping circle
+            float finalRadius = (float) Math.hypot(cx, cy);
+
+            // create the animator for this view (the start radius is zero)
+            Animator anim = ViewAnimationUtils.createCircularReveal(floatingActionButton, cx, cy, finalRadius, 0f);
+
+
+            anim.addListener(new AnimatorListenerAdapter() {          // listener to invisible the view after animation completes
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    floatingActionButton.setVisibility(View.INVISIBLE);
+                }
+            });
+
+            anim.start();
+
+        } else {
+            // set the view to invisible without a circular reveal animation below Lollipop
+            floatingActionButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    // revealing the fab button
+    public void revealFab(View view) {
+
+        // Check if the runtime version is at least Lollipop
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // get the center for the clipping circle
+            int cx = floatingActionButton.getWidth() / 2;
+            int cy = floatingActionButton.getHeight() / 2;
+
+            // get the final radius for the clipping circle
+            float finalRadius = (float) Math.hypot(cx, cy);
+
+            // create the animator for this view (the start radius is zero)
+            Animator anim = ViewAnimationUtils.createCircularReveal(floatingActionButton, cx, cy, 0f, finalRadius);
+
+            // make the view visible and start the animation
+            floatingActionButton.setVisibility(View.VISIBLE);
+            anim.start();
+
+        } else {
+            // set the view to invisible without a circular reveal animation below Lollipop
+            floatingActionButton.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
